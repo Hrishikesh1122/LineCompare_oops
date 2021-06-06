4aeab6256956adb540b6ff986dd0372c54b174a4
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-06 16:12:03 +0530

    Added : Welcome message

---
+++ Line Comparison Problem/src/linecompare/LineCompare.java
@@ -0,0 +1,17 @@
+package linecompare;
+
+public class LineCompare {
+	/**
+	 * Displays welcome message on console
+	 */
+	private static void displayWelcome() {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+	public static void main(String[] args) {
+		displayWelcome();
+		
+
+	}
+
+}
